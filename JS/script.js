df8cefd59ca04b608081f8d0123dbfb5a197bc43
unknown <parvezmosharraf.official@gmail.comm>
2022-08-01 15:44:35 +0600

    Project added

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+// Deposit 
+let depositValue = document.getElementById('deposit-value');
+let depositeOutput = document.getElementById('deposit-output')
+
+// Balance 
+let balanceOutput = document.getElementById('balance-output')
+
+// Withdraw  
+let withdrawValue = document.getElementById('withdraw-value');
+let withdrawOutput = document.getElementById('withdraw-output');
+
+function addMoney(currentMoney, NewMoney){
+    const result = Number(currentMoney) + Number(NewMoney);
+    return result;
+}
+
+function deposit(){
+ 
+    depositeOutput.innerText = 
+    Number(depositeOutput.innerText) + Number(depositValue.value);
+    // const totaloutput = addMoney(depositeOutput.innerText, depositValue.value)
+    // totalbalance = depositeOutput.innerText;
+
+
+
+    balanceOutput.innerText = 
+    Number (balanceOutput.innerText) + Number(depositValue.value);
+    // const balanceAmount = addMoney(balanceOutput.innerText, depositValue.value);
+    // balanceAmount = balanceOutput.innerText;
+    
+    depositValue.value = '';
+}
+ 
+
+// withdraw amount
+ function withdraw(){
+
+    withdrawOutput.innerText = 
+    Number(withdrawOutput.innerText) + Number(withdrawValue.value);
+
+    balanceOutput.innerText = 
+    Number (balanceOutput.innerText) - Number(withdrawValue.value);
+
+    
+    withdrawValue.value = '';
+}
